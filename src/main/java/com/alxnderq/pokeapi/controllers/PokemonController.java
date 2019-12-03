6840cb34f0f490a5c1c71b9a296fb9da469d67da
Alexander Quispe Alvi <alxnderq@gmail.com>
2019-12-03 17:28:12 -0500

    Add Swagger support

--- src/main/java/com/alxnderq/pokeapi/controllers/PokemonController.java
+++ src/main/java/com/alxnderq/pokeapi/controllers/PokemonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PokemonController {
 
-    @GetMapping(path = "/home")
+    @GetMapping(path = "/pokemon")
     public String home() {
         return "hola";
     }
---
+++ src/main/java/com/alxnderq/pokeapi/entity/Pokemon.java
@@ -0,0 +1,14 @@
+package com.alxnderq.pokeapi.entity;
+
+import lombok.Data;
+
+/**
+ * Pokemon
+ */
+@Data
+public class Pokemon {
+
+    private Integer id;
+    private String name;
+
+}
